85b71668d30079691ec6e499733bfc93ff4501fb
Carlos Cordova <carlosdung@hotmail.com>
2021-03-20 01:02:48 -0600

    proyecto base para empezar a trabajar, agregado webpack, babel,slint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+//import React from 'react';
+import React from "react";
+import '../assets/styles/App.scss';
+
+const HelloWorld = () =>(
+    <h1>Hello World :D</h1>
+)
+    
+
+export default HelloWorld;
